54562fb3dbad791af6571d01ed0e8fbd859063db
Mark Conrad <42453770+markC85@users.noreply.github.com>
2026-02-12 00:01:02 +0000

    updated with better RUFF settings to code reading and a check to make sure the user submits a correct model for the happy_model_options fuction

--- scr/job_scanner/llm/happy_client.py
+++ scr/job_scanner/llm/happy_client.py
@@ -6,17 +6,35 @@ from job_scanner.utils.logger_setup import start_logger
 
 LOG = start_logger()
 
-def happy_model_options(model_name:str)->str:
-    model_names = {
-        "meta_llama":"meta-llama/Llama-2-7b-chat-hf",
-        "mistralai":"mistralai/Mistral-7B-Instruct-v0.2"
-    }
+def happy_model_options(model_name:str) -> str:
+    """
+    This will return the model name for the happy client
 
+    Args:
+        model_name (str): this is the name of the model to use. Valid options are "meta_llama", "mistralai", and "NousResearch".
+
+    Returns:
+        model (str): this is the model name to use for the happy client
+    """
+    model_names = {
+        "meta_llama": "meta-llama/Llama-2-7b-chat-hf",
+        "mistralai": "mistralai/Mistral-7B-Instruct-v0.2",
+        "NousResearch": "NousResearch/Nous-Hermes-2-Mistral-7B",
+    }
     model = model_names.get(model_name)
+
+    if not model:
+        raise ValueError(f"Unknown model name '{model_name}'. Valid options: {list(model_names.keys())}")
 
     return model
 
-def set_up_hugging_env_var(json_path):
+def set_up_hugging_env_var(json_path) -> None:
+    """
+    This will set up the Hugging Face environment variable for the happy client
+
+    Args:
+        json_path (str): this is the path to the json file containing the Hugging Face token
+    """
     if "HF_TOKEN" not in os.environ:
         with open(json_path) as f:
             token = json.load(f).get("hugging_token")
@@ -27,20 +45,21 @@ def set_up_hugging_env_var(json_path):
     else:
         LOG.info("HF_TOKEN is all ready set up")
 
-def set_up_token(model_name: str):
+def set_up_token(model_name: str) -> tuple[str, AutoTokenizer, AutoModelForCausalLM]:
     """
     Set up the Happy client LLM so I can run it locally
-
     You set your 'speed' of the LLM here:
-
-
     for torch_dttype use torch.float32 for CPU base and accuracy
     for GPU and less accurate use torch.float16
-
 
     Args:
         model_name (str): this is the model to use
         memory_use (dtype):
+
+    Returns:
+        model_name (str): this is the model name used for the happy client
+        tokenizer (AutoTokenizer): this is the tokenizer for the model
+        model (AutoModelForCausalLM): this is the model for the happy client
     """
     model_name = happy_model_options(model_name)
 
@@ -56,7 +75,6 @@ def set_up_token(model_name: str):
         model_name,
         token=os.environ["HF_TOKEN"],
         quantization_config=bnb_config,
-        #device_map="cuda",
     )
     model.eval()
     model = torch.compile(model)
